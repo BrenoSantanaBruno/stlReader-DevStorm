15238740e1c70f97b71d26866b68fae6d806ecab
Breno Bruno <brenobruno@Brenos-MacBook-Pro-394.local>
2023-09-19 04:50:17 -0300

    Update Binary

--- challenge-server/main.go
+++ challenge-server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"mime/multipart"
 	"net/http"
+	"stlReader-DevStorm/stl"
 	"stlReader-DevStorm/web/handlers"
 )
 
@@ -13,6 +17,9 @@ func main() {
 
 	// Define a route for handling file uploads via POST requests
 	r.HandleFunc("/upload", handlers.UploadFileHandler).Methods("POST")
+
+	// Define a route for handling binary STL file processing
+	r.HandleFunc("/process-binary-stl", ProcessBinarySTLHandler).Methods("POST")
 
 	// Configure CORS policies
 	c := cors.New(cors.Options{
@@ -30,6 +37,56 @@ func main() {
 	handler := c.Handler(r)
 
 	// Set up an HTTP server to listen on port 8080
-	http.Handle("/", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server listening on :8080...")
+	err := http.ListenAndServe(":8080", handler)
+	if err != nil {
+		fmt.Println("Error starting the server:", err)
+	}
+}
+
+// ProcessBinarySTLHandler handles binary STL file processing
+func ProcessBinarySTLHandler(w http.ResponseWriter, r *http.Request) {
+	// Retrieve the uploaded file from the request
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer func(file multipart.File) {
+		err := file.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}(file)
+
+	// Process the binary STL file
+	_, numTriangles, err := stl.ProcessBinarySTL(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create a structure to store the results
+	type Result struct {
+		NumTriangles int `json:"numTriangles"`
+	}
+
+	result := Result{
+		NumTriangles: numTriangles,
+	}
+
+	// Convert the result structure to JSON
+	jsonResult, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set the response header to indicate JSON content
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Write the JSON response
+	w.Write(jsonResult)
 }
--- challenge-server/stl/stl_processing.go
+++ challenge-server/stl/stl_processing.go
@@ -2,53 +2,127 @@ package stl
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"math"
-	"mime/multipart"
+	"strconv"
 	"strings"
 )
 
+// Triangle represents a triangle in an STL file.
+type Triangle struct {
+	Vertices [3][3]float64
+}
+
 // ProcessSTLFile processes the STL file and returns the total area and the number of triangles.
-func ProcessSTLFile(file multipart.File) (float64, int, error) {
+func ProcessSTLFile(file io.Reader) (float64, int, error) {
+	isBinary, err := IsBinarySTL(file)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if isBinary {
+		return ProcessBinarySTL(file)
+	}
+
+	return ProcessASCIISTL(file)
+}
+
+// ProcessBinarySTL processes a binary STL file and returns the total area and the number of triangles.
+func ProcessBinarySTL(file io.Reader) (float64, int, error) {
+	// Read the 80-byte header (which can be ignored)
+	header := make([]byte, 80)
+	_, err := io.ReadFull(file, header)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Read the 4-byte triangle count (little-endian)
+	var numTriangles uint32
+	err = binary.Read(file, binary.LittleEndian, &numTriangles)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// Iterate through each triangle in the binary STL file
+	areaTotal := 0.0
+	for i := uint32(0); i < numTriangles; i++ {
+		// Read the 12 floats (3 vertices, each with X, Y, and Z) for each triangle
+		var vertices [3][3]float64
+		for j := 0; j < 3; j++ {
+			for k := 0; k < 3; k++ {
+				var coord float32
+				err := binary.Read(file, binary.LittleEndian, &coord)
+				if err != nil {
+					return 0, 0, err
+				}
+				// Convert from float32 to float64
+				vertices[j][k] = float64(coord)
+			}
+		}
+
+		// Calculate the area of the triangle and add it to the total area
+		area := CalculateTriangleArea(vertices)
+		areaTotal += area
+
+		// Skip the 2-byte attribute count
+		var attributeCount uint16
+		err := binary.Read(file, binary.LittleEndian, &attributeCount)
+		if err != nil {
+			return 0, 0, err
+		}
+	}
+
+	return areaTotal, int(numTriangles), nil
+}
+
+// ProcessASCIISTL processes an ASCII STL file and returns the total area and the number of triangles.
+func ProcessASCIISTL(file io.Reader) (float64, int, error) {
 	areaTotal := 0.0
 	numTriangles := 0
 
 	// Create a scanner to read lines from the input file
 	scanner := bufio.NewScanner(file)
 
-	// Iterate through each line of the STL file
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
+	var currentTriangle Triangle
 
-		// Check if the line starts with "facet normal"
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = trimSpaceAndToLower(line)
+
 		if strings.HasPrefix(line, "facet normal") {
 			numTriangles++
-
-			// Skip lines until "outer loop" is encountered
-			for scanner.Scan() && !strings.HasPrefix(strings.TrimSpace(scanner.Text()), "outer loop") {
-			}
-
-			// Initialize an array to store vertex coordinates
-			var vertices [3][3]float64
-
+		} else if strings.HasPrefix(line, "outer loop") {
 			// Read and parse each line of vertex coordinates
 			for i := 0; i < 3; i++ {
-				scanner.Scan()
-				parts := strings.Fields(scanner.Text())[1:] // Skip the first field
+				if !scanner.Scan() {
+					return 0, 0, fmt.Errorf("unexpected end of file")
+				}
+
+				line := scanner.Text()
+				line = trimSpaceAndToLower(line)
+				parts := strings.Fields(line)[1:] // Skip the first field
+				if len(parts) != 3 {
+					return 0, 0, fmt.Errorf("invalid vertex coordinates: %s", line)
+				}
+
 				for j, part := range parts {
 					// Parse and store the vertex coordinates
-					coord := strings.Replace(part, "e", "e+", 1)
-					fmt.Sscanf(coord, "%f", &vertices[i][j])
+					coord, err := strconv.ParseFloat(strings.Replace(part, "e", "e+", 1), 64)
+					if err != nil {
+						return 0, 0, err
+					}
+					currentTriangle.Vertices[i][j] = coord
 				}
 			}
 
 			// Calculate the area of the triangle and add it to the total area
-			area := CalculateTriangleArea(vertices)
+			area := CalculateTriangleArea(currentTriangle.Vertices)
 			areaTotal += area
 		}
 	}
 
-	// Check for scanner errors and return the results
 	if err := scanner.Err(); err != nil {
 		return 0, 0, err
 	}
@@ -86,4 +160,40 @@ func CalculateTriangleArea(vertices [3][3]float64) float64 {
 func Length(v [3]float64) float64 {
 	// Calculate the magnitude using the Pythagorean theorem
 	return math.Sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2])
+}
+
+// Helper function to trim spaces and convert to lowercase
+func trimSpaceAndToLower(s string) string {
+	return strings.ToLower(strings.TrimSpace(s))
+}
+
+// IsBinarySTL checks if a given STL file is in binary format.
+func IsBinarySTL(file io.Reader) (bool, error) {
+	// Read the first 5 bytes from the file
+	header := make([]byte, 5)
+	_, err := io.ReadAtLeast(file, header, 5)
+	if err != nil {
+		return false, err
+	}
+
+	// Check if the first 5 bytes contain "solid" keyword (ASCII STL) or a binary header
+	if string(header) == "solid" {
+		return false, nil // ASCII STL
+	}
+
+	// Check if the first 80 bytes contain an ASCII comment (text after "solid")
+	comment := make([]byte, 75) // 80 bytes minus "solid"
+	_, err = io.ReadAtLeast(file, comment, 75)
+	if err != nil {
+		return false, err
+	}
+
+	// Check if the comment section is empty or contains only whitespaces
+	for _, char := range comment {
+		if char != ' ' && char != '\t' && char != '\n' && char != '\r' {
+			return true, nil // Binary STL
+		}
+	}
+
+	return false, nil // ASCII STL (no comment)
 }
